a07e3e130595e3872f736f96270fee698192a0c1
Isoken June Ibizugbe <isokenjune@gmail.com>
2023-12-21 01:09:37 +0100

    added tests

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,69 +1,92 @@
 #!/usr/bin/python3
-""" """
+"""Tests place module """
 from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
+import os
 
 
 class test_Place(test_basemodel):
-    """ """
+    """Tests place """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Public initializer """
         super().__init__(*args, **kwargs)
         self.name = "Place"
         self.value = Place
 
     def test_city_id(self):
-        """ """
+        """Tests city_id attribute """
         new = self.value()
-        self.assertEqual(type(new.city_id), str)
+        self.assertEqual(type(new.city_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_user_id(self):
-        """ """
+        """Tests user_id attribute """
         new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        self.assertEqual(type(new.user_id), str  if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_name(self):
-        """ """
+        """Tests place name attribute """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertEqual(type(new.name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_description(self):
-        """ """
+        """Tests place description """
         new = self.value()
-        self.assertEqual(type(new.description), str)
+        self.assertEqual(type(new.description), str  if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_number_rooms(self):
-        """ """
+        """Tests place number of rooms """
         new = self.value()
-        self.assertEqual(type(new.number_rooms), int)
+        self.assertEqual(type(new.number_rooms), int if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_number_bathrooms(self):
-        """ """
+        """Tests place numb of bathrooms """
         new = self.value()
-        self.assertEqual(type(new.number_bathrooms), int)
+        self.assertEqual(type(new.number_bathrooms), int if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_max_guest(self):
-        """ """
+        """Tets max_guest attribute """
         new = self.value()
-        self.assertEqual(type(new.max_guest), int)
+        self.assertEqual(type(new.max_guest), int if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_price_by_night(self):
-        """ """
+        """Tests place night price """
         new = self.value()
-        self.assertEqual(type(new.price_by_night), int)
+        self.assertEqual(type(new.price_by_night), int  if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_latitude(self):
-        """ """
+        """Tests place latitude """
         new = self.value()
-        self.assertEqual(type(new.latitude), float)
+        self.assertEqual(type(new.latitude), float  if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_longitude(self):
-        """ """
+        """Tests place longitude """
         new = self.value()
-        self.assertEqual(type(new.latitude), float)
+        self.assertEqual(type(new.latitude), float if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_amenity_ids(self):
-        """ """
+        """Tests amenity ids """
         new = self.value()
-        self.assertEqual(type(new.amenity_ids), list)
+        self.assertEqual(type(new.amenity_ids), list  if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,42 @@
 #!/usr/bin/python3
-""" """
+"""Tests user module """
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
-
+import os
 
 class test_User(test_basemodel):
-    """ """
+    """Tests user module """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Public initializer """
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """ """
+        """Tests user firstname """
         new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        self.assertEqual(type(new.first_name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_last_name(self):
-        """ """
+        """Tests user last_name """
         new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        self.assertEqual(type(new.last_name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_email(self):
-        """ """
+        """Tests email """
         new = self.value()
-        self.assertEqual(type(new.email), str)
+        self.assertEqual(type(new.email), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_password(self):
-        """ """
+        """Tets user passsword """
         new = self.value()
-        self.assertEqual(type(new.password), str)
+        self.assertEqual(type(new.password), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
