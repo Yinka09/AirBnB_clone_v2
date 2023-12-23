64d98cad5f0268686f62f645b85cbd7a0902fb53
Isoken June Ibizugbe <isokenjune@gmail.com>
2023-12-23 13:55:16 +0100

    tests update 2

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,8 +3,6 @@
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
 import os
-import unittest
-from datetime import datetime
 
 
 class test_User(test_basemodel):
